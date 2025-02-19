22532f0c0aa2690b7d9dc90cea4e09d465955bfb
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2025-02-19 17:07:13 -0500

    Create starter files (#13)
    * Create project template files
    
    * Allow project to run

---
+++ src/main/java/com/myactivityplanner/enterprise/MyActivityPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.myactivityplanner.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyActivityPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyActivityPlannerApplication.class, args);
+    }
+
+}
