e2d05f96f22cbf342bbab147b2e7c60f3767c2b1
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2025-02-19 16:56:36 -0500

    Create project template files

---
+++ src/test/java/com/myactivityplanner/enterprise/MyActivityPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myactivityplanner.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyActivityPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
