f74059fa755d800ba1636365d35651e7de197424
schmidev <156551005+schmidev@users.noreply.github.com>
2025-03-01 19:03:02 -0500

    Controller interface (#20)
    * added file for interface controller (no business logic)
    
    * deleted Controllers.java; moved content to ActivityController.java
    
    * Added import to fix @GetMapping errors
    
    * Removed ActivityController copy
    
    * Removed all interface... things
    
    * Added a bunch of '()'
    
    * imported LocalDate
    
    * added return statements
    
    * i'm starting to despise this Maven thing
    
    * aaaaaaaaaa
    
    * put everything in annotations
    
    * aaaa

--- src/main/java/com/myactivityplanner/enterprise/ActivityController.java
+++ src/main/java/com/myactivityplanner/enterprise/ActivityController.java
@@ -2,6 +2,10 @@ package com.myactivityplanner.enterprise;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import java.time.LocalDate;
+
 
 @Controller
 public class ActivityController {
@@ -14,4 +18,58 @@ public class ActivityController {
     public String index() {
         return "index";
     }
+/**
+ *     @GetMapping
+    public LocalDate getActivitiesByDate() {
+        /**
+         * Retrieves a list of activities on a specific date
+         * Or a length of dates?
+         * Will request for date and
+         * @param date of the activity/ies
+         * @return a list of activities occurring on the given date
+         *
+       * return int LocalDate;
+    }
+    
+    @GetMapping
+    public int getUsersByActivity() {
+    /**
+     * Retrieves a list of users that have signed up for a given activity
+     * @param activityID
+     * @return a list of users(userID) for the specified activity (array?)
+     *
+    return int userID;
+    }
+    
+    @GetMapping
+    public boolean signUpForActivity() {
+        /**
+         * @param activityID
+         * @param userID
+         * @return boolean is True for user being signed up for activity
+         *
+        *return true;
+    }
+
+    @GetMapping
+    public boolean isUserSignedUpForActivity() {
+        /**
+         * @param activityID
+         * @param userID
+         * @return boolean confirming a user is registered for an activity
+         *
+        return true;
+    }
+
+    @DeleteMapping
+    public boolean withdrawFromActivity() {
+        /**
+         * Allows user to withdraw from an activity
+         * @param activityID
+         * @param userID
+         * @return removal of user from activity list of specified activity
+         *
+        return false;
+    }
+    */
 }
