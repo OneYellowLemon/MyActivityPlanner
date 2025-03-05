7f532ee02623dd6e287a6e4f31bc281010b67433
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2025-03-05 10:49:56 -0500

    Create first controllers (#25)

--- src/main/java/com/myactivityplanner/enterprise/ActivityController.java
+++ src/main/java/com/myactivityplanner/enterprise/ActivityController.java
@@ -1,75 +1,66 @@
 package com.myactivityplanner.enterprise;
 
+import com.myactivityplanner.enterprise.dto.Activity;
+import com.myactivityplanner.enterprise.service.IActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import java.time.LocalDate;
-
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ActivityController {
 
+    @Autowired
+    IActivityService activityService;
+
     /**
      * Handle the root (/) endpoint and return a start page
-     * @return index.html
+     * @return index.html UI page
      */
     @RequestMapping
     public String index() {
         return "index";
     }
-/**
- *     @GetMapping
-    public LocalDate getActivitiesByDate() {
-        /**
-         * Retrieves a list of activities on a specific date
-         * Or a length of dates?
-         * Will request for date and
-         * @param date of the activity/ies
-         * @return a list of activities occurring on the given date
-         *
-       * return int LocalDate;
-    }
-    
-    @GetMapping
-    public int getUsersByActivity() {
+
     /**
-     * Retrieves a list of users that have signed up for a given activity
-     * @param activityID
-     * @return a list of users(userID) for the specified activity (array?)
-     *
-    return int userID;
-    }
-    
-    @GetMapping
-    public boolean signUpForActivity() {
-        /**
-         * @param activityID
-         * @param userID
-         * @return boolean is True for user being signed up for activity
-         *
-        *return true;
+     * API endpoint to get activity by ID
+     * @param id The ID of the activity to get
+     * @return The activity with the given ID
+     */
+    @GetMapping("api/activity/{id}")
+    @ResponseBody
+    public Activity getActivity(@PathVariable int id) {
+        return activityService.getActivity(id);
     }
 
-    @GetMapping
-    public boolean isUserSignedUpForActivity() {
-        /**
-         * @param activityID
-         * @param userID
-         * @return boolean confirming a user is registered for an activity
-         *
-        return true;
+    /**
+     * API endpoint to save an activity
+     * @param activity The activity to save
+     * @return True if the activity was saved successfully, otherwise false
+     */
+    @PutMapping("api/activity")
+    public boolean saveActivity(@RequestBody Activity activity) {
+        return activityService.saveActivity(activity);
     }
 
-    @DeleteMapping
-    public boolean withdrawFromActivity() {
-        /**
-         * Allows user to withdraw from an activity
-         * @param activityID
-         * @param userID
-         * @return removal of user from activity list of specified activity
-         *
-        return false;
+    /**
+     * API endpoint to delete an activity
+     * @param id The ID of the activity to delete
+     * @return True if the activity was deleted successfully, otherwise false
+     */
+    @DeleteMapping("api/activity/{id}")
+    public boolean deleteActivity(@PathVariable int id) {
+        return activityService.deleteActivity(id);
     }
-    */
+
+    /**
+     * API endpoint to check if user is signed up for activity
+     * @param activityId The ID of the activity to check
+     * @param userId The ID of the user to check
+     * @return True if the user is signed up for the activity, otherwise false
+     */
+    @GetMapping("api/activity/signedup/{activityId}/{userId}")
+    @ResponseBody
+    public boolean isUserSignedUpForActivity(@PathVariable int activityId, @PathVariable int userId) {
+        return activityService.isUserSignedUpForActivity(userId, activityId);
+    }
 }
