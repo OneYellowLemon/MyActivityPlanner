4b3427b340164851a69a81a120b75cb54d8fb072
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2025-03-05 10:33:18 -0500

    Fix service stubs not annotated properly

--- src/main/java/com/myactivityplanner/enterprise/service/ActivityServiceStub.java
+++ src/main/java/com/myactivityplanner/enterprise/service/ActivityServiceStub.java
@@ -2,14 +2,12 @@ package com.myactivityplanner.enterprise.service;
 
 import com.myactivityplanner.enterprise.dto.Activity;
 import com.myactivityplanner.enterprise.dto.User;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 
-@Repository
-@Profile("test")
+@Service
 public class ActivityServiceStub implements IActivityService {
 
     @Override
