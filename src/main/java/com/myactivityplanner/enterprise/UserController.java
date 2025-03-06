8f1374013656612e31e62486adeda52a06707a3f
schmidev <156551005+schmidev@users.noreply.github.com>
2025-03-05 22:05:16 -0500

    added UserController Interface (#27)
    * added UserController Interface
    
    * corrected spelling error
    
    * altered mapping
    
    * changed first GetMapping to PutMapping
    
    * removed requestmapping

---
+++ src/main/java/com/myactivityplanner/enterprise/UserController.java
@@ -0,0 +1,25 @@
+package com.myactivityplanner.enterprise;
+
+import com.myactivityplanner.enterprise.dto.Activity;
+import com.myactivityplanner.enterprise.dto.User;
+import com.myactivityplanner.enterprise.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+    
+    @Autowired
+    IUserService userService;
+
+    @PutMapping("api/user")
+    public boolean createUser(@PathVariable String firstName, @PathVariable String lastName) {
+        return userService.createUser(firstName, lastName);
+    }
+
+    @GetMapping("api/user/{userId}")
+    public String getUserName(@PathVariable int userId) {
+        return userService.getUserName(userId);
+    }
+}
