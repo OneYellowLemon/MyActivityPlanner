8f5e878c486a5be13a2026df672eb39392a209ea
schmidev <evanrsc@gmail.com>
2025-03-05 20:47:04 -0500

    implemented IUserActivityService?

--- src/main/java/com/myactivityplanner/enterprise/ActivityController.java
+++ src/main/java/com/myactivityplanner/enterprise/ActivityController.java
@@ -3,6 +3,7 @@ package com.myactivityplanner.enterprise;
 import com.myactivityplanner.enterprise.dto.Activity;
 import com.myactivityplanner.enterprise.dto.User;
 import com.myactivityplanner.enterprise.service.IActivityService;
+import com.myactivityplanner.enterprise.service.IUserActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,9 @@ public class ActivityController {
 
     @Autowired
     IActivityService activityService;
+
+    @Autowired
+    IUserActivityService userActivityService;
 
     /**
      * Handle the root (/) endpoint and return a start page
@@ -66,32 +70,32 @@ public class ActivityController {
     @GetMapping("api/activity/signedup/{activityId}/{userId}")
     @ResponseBody
     public boolean isUserSignedUpForActivity(@PathVariable int activityId, @PathVariable int userId) {
-        return activityService.isUserSignedUpForActivity(userId, activityId);
+        return userActivityService.isUserSignedUpForActivity(userId, activityId);
     }
     
     @GetMapping("api/activity/getUsersSignedUp/{activityId}")
     public List<User> getUsersSignedUpForActivity(@PathVariable int activityId) {
-        return activityService.getUsersSignedUpForActivity(activityId);
+        return userActivityService.getUsersSignedUpForActivity(activityId);
     }
 
     @GetMapping("api/activity/activitiesForDate/{timestamp}")
     public List<Activity> getActivitiesForDate(@PathVariable Date timestamp) {
-        return activityService.getActivitiesForDate(timestamp);
+        return userActivityService.getActivitiesForDate(timestamp);
     }
 
     @GetMapping("api/activity/activitiesUserSignedUpFor/{userId}")
     public List<Activity> getActivitiesUserIsSignedUpFor(@PathVariable int userId) {
-        return activityService.getActivitiesUserIsSignedUpFor(userId);
+        return userActivityService.getActivitiesUserIsSignedUpFor(userId);
     }
 
     @GetMapping("api/activity/signUpUser/{activityId}/{userId}")
     @ResponseBody
     public boolean signUpUser(@PathVariable int activityId, @PathVariable int userId) {
-        return activityService.signUpUser(activityId, userId);
+        return userActivityService.signUpUser(activityId, userId);
     }
 
     @DeleteMapping("api/activity/withdrawUser/{activityId}/{userId}")
     public boolean withdrawUser(@PathVariable int activityId, @PathVariable int userId) {
-        return activityService.withdrawUser(activityId, userId);
+        return userActivityService.withdrawUser(activityId, userId);
     }
 }
