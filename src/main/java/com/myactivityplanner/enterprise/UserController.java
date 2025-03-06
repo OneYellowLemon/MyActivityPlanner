9b163c427f0cdd50f8cfe318b2a0601a34d2b6a4
schmidev <evanrsc@gmail.com>
2025-03-05 21:53:13 -0500

    added UserController Interface

---
+++ src/main/java/com/myactivityplanner/enterprise/UserController.java
@@ -0,0 +1,30 @@
+package com.myactivityplanner.enterprise;
+
+import com.myactivityplanner.enterprise.dto.Activity;
+import com.myactivityplanner.enterprise.dto.User;
+import com.myactivityplanner.enterprise.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+    
+    @Autowired
+    IUserService userService;
+    
+    @RequestMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("api/user/{firstName}/{lastName}")
+    public boolean createUser(@PathVariable String firstName, @PathVariable String lastName) {
+        return userService.createUser(firstname, lastName);
+    }
+
+    @GetMapping("api/user/{userId}")
+    public String getUserName(@PathVariable int userId) {
+        return userService.getUserName(userId);
+    }
+}
