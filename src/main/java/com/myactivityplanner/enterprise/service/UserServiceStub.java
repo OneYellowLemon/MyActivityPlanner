180c83ae73aea94da4958f40e4803b83bbaa3617
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2025-03-24 16:41:38 -0400

    Fix profile on UserServiceStub

--- src/main/java/com/myactivityplanner/enterprise/service/UserServiceStub.java
+++ src/main/java/com/myactivityplanner/enterprise/service/UserServiceStub.java
@@ -1,11 +1,9 @@
 package com.myactivityplanner.enterprise.service;
 
 import com.myactivityplanner.enterprise.dto.User;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("test")
 public class UserServiceStub implements IUserService {
 
     @Override
