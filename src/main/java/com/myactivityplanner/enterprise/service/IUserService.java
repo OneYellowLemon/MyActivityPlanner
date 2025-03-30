912e6eb1e9853b784eeb868e90d9c541e4b54dae
schmidev <156551005+schmidev@users.noreply.github.com>
2025-03-30 17:24:04 -0400

    Created UserService.java (#44)
    * Created UserService.java
    
    * Made minor change in UserController
    
    * Fixed getUserName. Should be looked at further corrections

--- src/main/java/com/myactivityplanner/enterprise/service/IUserService.java
+++ src/main/java/com/myactivityplanner/enterprise/service/IUserService.java
@@ -14,6 +14,7 @@ public interface IUserService {
 
     /**
      * Get the full name of a user
+     *
      * @param userId The ID of the user to get the name of
      * @return The full name of the user
      * @throws Exception If an error occurs
---
+++ src/main/java/com/myactivityplanner/enterprise/service/UserService.java
@@ -0,0 +1,22 @@
+package com.myactivityplanner.enterprise.service;
+
+import com.myactivityplanner.enterprise.dao.IUserDAO;
+import com.myactivityplanner.enterprise.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+    @Autowired
+    private IUserDAO userDAO;
+    @Override
+    public User createUser(String firstName, String lastName) throws Exception {
+        return userDAO.createUser(firstName, lastName);
+    }
+
+    @Override
+    public String getUserName(int userId) throws Exception {
+        userDAO.getUserName(userId);
+        return "test user";
+    }
+}
