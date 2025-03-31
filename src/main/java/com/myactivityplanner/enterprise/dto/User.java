aa54bff82cd68d4c4dfb075f7c739c8412ac1e2c
Emmana21 <156630451+Emmana21@users.noreply.github.com>
2025-03-30 23:13:19 -0400

    Update User.java
    Edits/ comments

--- src/main/java/com/myactivityplanner/enterprise/dto/User.java
+++ src/main/java/com/myactivityplanner/enterprise/dto/User.java
@@ -2,9 +2,25 @@ package com.myactivityplanner.enterprise.dto;
 
 import lombok.Data;
 
+/**
+ * A simple DTO class representing a User.
+ * It contains basic information such as user ID, first name, and last name.
+ */
 @Data
 public class User {
+
+    /**
+     * The unique identifier for the user.
+     */
     private int userId;
+
+    /**
+     * The first name of the user.
+     */
     private String firstName;
+
+    /**
+     * The last name of the user.
+     */
     private String lastName;
 }
