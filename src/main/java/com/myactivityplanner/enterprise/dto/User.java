d2c67f9a6439f49231507abd77abba484e53156a
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2025-04-14 17:25:15 -0400

    Set database column length properties

--- src/main/java/com/myactivityplanner/enterprise/dto/User.java
+++ src/main/java/com/myactivityplanner/enterprise/dto/User.java
@@ -1,5 +1,6 @@
 package com.myactivityplanner.enterprise.dto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +13,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
+    @Column(length = 50)
     private String firstName;
+    @Column(length = 50)
     private String lastName;
 }
